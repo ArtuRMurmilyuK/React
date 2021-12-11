d250b3615dc70462a9bea498b562bb41ba31e3df
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-12-11 11:51:02 +0200

    Create the project

---
+++ react-sneakers-drop/src/App.js
@@ -0,0 +1,28 @@
+function App() {
+  return <div className="wrapper">
+    <header>
+    <div className="headerLeft">
+      <svg/>
+      <div className="hederInfo">
+        <h3>PavlovKross</h3>
+        <p>🖤Кроссовки как стиль🖤</p>
+      </div>
+    </div>
+      <ul className="headerRight">
+        <li>
+          <svg />
+          <span>1000grn</span>
+        </li>
+        <li>
+          <svg/>
+
+        </li>
+      </ul>
+    </header>
+    <div className="content">
+      <h1>Все кроссовки</h1>
+    </div>
+  </div>;
+}
+
+export default App;
